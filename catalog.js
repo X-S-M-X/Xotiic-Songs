eb71a40b1ce604c82410c5db00c05cfaedb31dc2
K <exshadowzz1@gmail.com>
2026-08-01 23:04:22 +1200

    Publish Void Crown Treasury

--- catalog.js
+++ catalog.js
@@ -1,2 +1,15 @@
 // Managed by Xotiic Upload. Only complete releases are shown publicly.
-window.XOTIICDUCK_RELEASES = [];
+window.XOTIICDUCK_RELEASES = [
+  {
+    "id": "void-crown-treasury",
+    "title": "Void Crown Treasury",
+    "artist": "XotiicDuck",
+    "album": "Single",
+    "genre": "Anime J-Rock",
+    "releaseDate": "2026-08-01",
+    "duration": 306,
+    "audio": "music/void-crown-treasury.mp3",
+    "cover": "covers/void-crown-treasury.png",
+    "status": "published"
+  }
+];
